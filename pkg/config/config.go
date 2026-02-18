43c4f8de590f662fa7c73dc2634762a9ff959f4f
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 15:59:20 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -16,6 +16,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the Calendly API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 )
 
